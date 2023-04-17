2f08e5e4d958b92e8825d0ebea0ad53ebcd9bbe7
Ghislain Vaillant <ghisvail@gmail.com>
2023-04-18 00:07:32 +0200

    NEW: Initial commit of get_template task

--- src/pydra/tasks/templateflow/__init__.py
+++ src/pydra/tasks/templateflow/__init__.py
@@ -2,3 +2,24 @@
 
 >>> from pydra.tasks import templateflow 
 """
+
+__all__ = ["get_template"]
+
+from templateflow import api as _api
+
+import pydra
+
+
+@pydra.mark.task
+def get_template(template_id: str, output_entities: dict) -> dict:
+    """Get template using the TemplateFlow Python client."""
+
+    output_entities = output_entities or {}
+
+    return dict(
+        {"template_description": _api.get_metadata(template=template_id)},
+        **{
+            output: _api.get(template=template_id, **entities)
+            for output, entities in output_entities.items()
+        }
+    )
