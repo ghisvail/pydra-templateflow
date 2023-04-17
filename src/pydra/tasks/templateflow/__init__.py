47c82b288b5250f99848ef7984abb9db62f5bcb0
Ghislain Vaillant <ghisvail@gmail.com>
2023-04-18 01:19:15 +0200

    DOC: Add docstring and doctest

--- src/pydra/tasks/templateflow/__init__.py
+++ src/pydra/tasks/templateflow/__init__.py
@@ -1,7 +1,4 @@
-"""Pydra tasks for TemplateFlow.
-
->>> from pydra.tasks import templateflow 
-"""
+"""Pydra tasks for TemplateFlow."""
 
 __all__ = ["get_template"]
 
@@ -12,7 +9,28 @@ import pydra
 
 @pydra.mark.task
 def get_template(template_id: str, output_queries: dict) -> dict:
-    """Get template using the TemplateFlow Python client."""
+    """Get template using the TemplateFlow Python client.
+
+    Parameters
+    ----------
+    template_id : str
+        Template identifier to download.
+    output_queries : dict
+        Mapping of output names to file queries.
+
+    Returns
+    -------
+    template_description : dict
+        Metadata associated with the template.
+    **output_names : path-like
+        Path to the downloaded file for each query.
+
+    Examples
+    --------
+    >>> task = get_template(template_id="MNI152Lin")
+    >>> task()  # doctest: +ELLIPSIS
+    Result(...)
+    """
 
     output_queries = output_queries or {}
 
