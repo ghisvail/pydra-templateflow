84e449585fbb9fd2e7bc105fd39422a911a6f2f0
Ghislain Vaillant <ghisvail@gmail.com>
2023-04-18 13:06:18 +0200

    ENH: Return template description and files as separate output fields

--- src/pydra/tasks/templateflow/__init__.py
+++ src/pydra/tasks/templateflow/__init__.py
@@ -2,39 +2,62 @@
 
 __all__ = ["get_template"]
 
+import functools
+import os
+
 from templateflow import api as _api
 
 import pydra
 
 
-@pydra.mark.task
-def get_template(template_id: str, output_queries: dict) -> dict:
+def _get_template(template_id: str, output_queries: dict) -> dict:
+    return dict(
+        {"template_description": _api.get_metadata(template=template_id)},
+        **{output: _api.get(template=template_id, **entities) for output, entities in output_queries.items()},
+    )
+
+
+def get_template(output_queries: dict, **kwargs) -> pydra.engine.task.FunctionTask:
     """Get template using the TemplateFlow Python client.
 
     Parameters
     ----------
-    template_id : str
-        Template identifier to download.
     output_queries : dict
         Mapping of output names to file queries.
+    kwargs : dict
+        Task parameters.
 
     Returns
     -------
-    template_description : dict
-        Metadata associated with the template.
-    **output_names : path-like
-        Path to the downloaded file for each query.
+    pydra.engine.task.FunctionTask
+        Pydra task for querying template files.
 
     Examples
     --------
-    >>> task = get_template(template_id="MNI152Lin")
-    >>> task()  # doctest: +ELLIPSIS
-    Result(...)
+    >>> task = get_template(template_id="MNI152Lin", output_queries={"t1w": {"resolution": "1", "suffix": "T1w"}})
+    >>> result = task()
+    >>> result.output.template_description  # doctest: +ELLIPSIS
+    {... 'Name': 'Linear ICBM Average Brain (ICBM152) Stereotaxic Registration Model', ...}
+    >>> result.output.t1w  # doctest: +ELLIPSIS
+    PosixPath('.../tpl-MNI152Lin_res-01_T1w.nii.gz')
     """
 
     output_queries = output_queries or {}
 
-    return dict(
-        {"template_description": _api.get_metadata(template=template_id)},
-        **{output: _api.get(template=template_id, **entities) for output, entities in output_queries.items()},
+    name = kwargs.pop("name", "get_template")
+
+    input_spec = pydra.specs.SpecInfo(name="Input", fields=[("template_id", str)], bases=(pydra.specs.BaseSpec,))
+
+    output_spec = pydra.specs.SpecInfo(
+        name="Output",
+        fields=[("template_description", dict)] + [(str(key), os.PathLike) for key in output_queries.keys()],
+        bases=(pydra.specs.BaseSpec,),
+    )
+
+    return pydra.engine.task.FunctionTask(
+        func=functools.partial(_get_template, output_queries=output_queries),
+        name=name,
+        input_spec=input_spec,
+        output_spec=output_spec,
+        **kwargs,
     )
